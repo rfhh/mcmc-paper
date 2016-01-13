3abc70bbb5942c7657bd6be2cca04f3c8f088d71
Ismail El Helw <ielhelw@cs.vu.nl>
2016-01-13 13:16:44 +0100

    Added evaluation intro, DAS5 description and fixed some captions

--- plots/plot-ml-curves.sh
+++ plots/plot-ml-curves.sh
@@ -2,32 +2,56 @@
 
 cat <<EOF | gnuplot --persist
 
-set terminal postscript eps enhanced color font ',8' size 3.3,1.6
+#set terminal postscript eps enhanced color font ',8' size 3.3,1.6
 
 set xlabel "Time (seconds)"
 set ylabel "Perplexity"
-set xrange [0:]
+#set xrange [0:]
 set nokey
 set grid ytics
 
+w = 0.6
+h = 0.4
+k = 0.0
+nr = 3
+nc = 2
+row(x) = ((nr-x-1)*h)+k
+col(x) = x*w
 
-set output 'ppx-amazon.eps'
-set title "com-Amazon | K75149-m1K-n32-msx1K-np24"
+set terminal postscript eps enhanced color font ',8'
+set size (nc*w),(nr*h+k)
+set output 'ppx.eps'
+set multiplot layout nr,nc
+set size w,h
+
+
+set origin col(0),row(0)
+set size w,h
+#set output 'ppx-amazon.eps'
+set title "(a) com-Amazon | K75149-m1K-n32-msx1K-np24"
 plot '< grep average_count ../data/ml-curves/log-amazon-K75149-m1K-n32-msx1K-epst1e-04-np24/log.0' u 8:(exp(\$14)) w l
 
-set output 'ppx-lj.eps'
-set title "com-LiveJournal | K192K-flt32-m1K-n32-mxs1K-x1M-np65"
+set origin col(1),row(0)
+set size w,h
+#set output 'ppx-lj.eps'
+set title "(b) com-LiveJournal | K192K-flt32-m1K-n32-mxs1K-x1M-np65"
 plot '< grep average_count ../data/ml-curves/log-com-LJ-K192K-flt32-m1K-n32-mxs1K-x1M-np65/log.0' u 8:(exp(\$14)) w l
 
-set output 'ppx-dblp.eps'
-set title "com-DBLP | K13477-flt32-m1K-n32-msx4K-x10M-np24"
+set origin col(0),row(1)
+set size w,h
+#set output 'ppx-dblp.eps'
+set title "(c) com-DBLP | K13477-flt32-m1K-n32-msx4K-x10M-np24"
 plot '< grep average_count ../data/ml-curves/log-dblp-K13477-flt32-m1K-n32-msx1K-x10M-epst1e-05-np24/log.0' u 8:(exp(\$14)) w l
 
-set output 'ppx-friendster.eps'
-set title "com-Friendster | K12K-flt32-m16K-n32-mxs-16K-np65"
+set origin col(1),row(1)
+set size w,h
+#set output 'ppx-friendster.eps'
+set title "(d) com-Friendster | K12K-flt32-m16K-n32-mxs-16K-np65"
 plot '< grep average_count ../data/ml-curves/log-Friendster-K12K-flt32-m16K-n32-mxs-16K-np65/log.0' u 8:(exp(\$14)) w l
 
-set output 'ppx-youtube.eps'
-set title "com-Youtube | K8385-m1K-n32-msx1K-np15"
+set origin col(0),row(2)
+set size w,h
+#set output 'ppx-youtube.eps'
+set title "(e) com-Youtube | K8385-m1K-n32-msx1K-np15"
 plot '< grep average_count ../data/ml-curves/log-youtube-K8385-m1K-n32-msx1K-np15/log.0' u 8:(exp(\$14)) w l
 EOF
