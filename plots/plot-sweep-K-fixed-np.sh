7374c3a7954d686dd84eab1f687a39a4d2b6ce6c
Rutger Hofman <rutger@cs.vu.nl>
2016-01-21 09:56:28 +0100

    Work on 04 pipeline. Remove 04 weak scaling Fig 2(b)

--- plots/plot-sweep-K-fixed-np.sh
+++ plots/plot-sweep-K-fixed-np.sh
@@ -33,7 +33,7 @@ set grid ytics
 set output 'sweep-over-K-fixed-np.eps'
 set title "Scaling K (fixed np=65)"
 set key left
-plot "$$.tmp" u 1:2 w lp t "Single buffering", "$$.tmp2" u 1:2 w lp t "Double buffering"
+plot "$$.tmp" u 1:2 w lp t "Pipelining disabled", "$$.tmp2" u 1:2 w lp t "Pipelining enabled"
 
 EOF
 
--- plots/plot-sweep-K-proportional-np.sh
+++ plots/plot-sweep-K-proportional-np.sh
@@ -10,31 +10,15 @@ grep iteration ../data/sweep-over-K-proportional-np/*/* \
   | bash \
   | sort -n > $$.tmp
 
-ls ../data/sweep-over-K-proportional-np/*/* \
-  | grep -wv np5 \
-  | sed 's/.*-K\([0-9K]\+\).*-np\([0-9]\+\).*/\2 \1/' \
-  | sed 's/\([0-9]\+\)K/$[\1*1024]/' \
-  | sed 's/\(.*\)/echo \1/' \
-  | bash \
-  | sort -n \
-  | awk '{print $1-1 " " $2}' > $$.tmp.2
-
 cat $$.tmp
 
 cat <<EOF | gnuplot --persist
 
 w = 0.6
 h = 0.4
-k = 0.0
-nr = 2
-nc = 1
-row(x) = ((nr-x-1)*h)+k
-col(x) = x*w
 
 set terminal postscript eps enhanced color font ',8'
-set size (nc*w),(nr*h+k)
 set output 'sweep-over-K-proportional-np.eps'
-set multiplot layout nr,nc
 set size w,h
 
 set xlabel "Number of Nodes"
@@ -43,27 +27,9 @@ set yrange[0:500]
 set nokey
 set grid ytics
 
-set origin col(0),row(0)
-set size w,h
-set title "(a) Weak Scaling (over K)"
+set title "Weak Scaling (over K)"
 set ylabel "Time per Iteration (milliseconds)"
 plot "$$.tmp" u (\$2-1):(1000*\$3/\$4) w lp axis x1y1
-
-set origin col(0),row(1)
-set size w,h
-set style data histogram 
-set style histogram cluster gap 1
-
-set style fill solid noborder
-set auto x
-set ytics
-set mytics
-set grid ytics mytics
-
-set yrange[0:*]
-set title "(b) Number of communities used per cluster size"
-set ylabel "#K"
-plot "$$.tmp.2" u 2:xtic(1)
 
 EOF
 
