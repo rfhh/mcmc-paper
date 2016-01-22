78b765a4d974665e0c83bb143a1a13b6126c828b
Rutger Hofman <rutger@cs.vu.nl>
2016-01-22 16:24:17 +0100

    Add MPI roundtrip times

--- plots/plot-qperf.sh
+++ plots/plot-qperf.sh
@@ -3,14 +3,15 @@ cat <<EOI | gnuplot --persist
 set terminal postscript eps enhanced color font ',8' size 3.3,1.6
 set output 'qperf.eps'
 
-set logscale xy
-set xrange [:2000000]
-set yrange [0.25:10]
-set key at 1000000,1
+set logscale x
+# set xrange [:2000000]
+# set yrange [0.25:10]
+set key at 1000000,3
 set xlabel "payload size (bytes)"
 set ylabel "throughput (GB/s)"
 plot \
 	"../data/qperf/qperf/qperf-write-bw" with lp title "qperf RDMA write", \
 	"../data/qperf/qperf/qperf-read-bw" with lp title "qperf RDMA read", \
-	"../data/qperf/dkv/thrp" with lp title "qperf DKV store read"
+	"../data/qperf/dkv/thrp" with lp title "DKV store read", \
+	"../data/qperf/mpi/thrp" with lp title "MPI read"
 EOI
