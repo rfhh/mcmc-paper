15c72729981d0abb1660e758b689171266d5dd10
Rutger Hofman <rutger@cs.vu.nl>
2017-05-25 17:12:47 +0200

    Start removing the OpenCL CPU work

--- plots/plot-gpus.sh
+++ plots/plot-gpus.sh
@@ -8,7 +8,8 @@ BASELINE=374.667
 echo > $TFILE
 
 X=1
-for i in GeForceGTXTITANX GeForceGTX980 TeslaK40c TeslaK20m Intel-R-Xeon; do
+# for i in GeForceGTXTITANX GeForceGTX980 TeslaK40c TeslaK20m Intel-R-Xeon; do
+for i in GeForceGTXTITANX GeForceGTX980 TeslaK40c TeslaK20m ; do
   v=`grep -a TOTAL dblp-K1024/${i}* | sort -nk 7 | head -n 1 | awk '{print $7}'`
   v2=`perl -e "print $BASELINE / $v"`
   device=`echo $i | sed 's/GeForceGTXTITANX/Titan-X/;s/GeForceGTX980/GTX980/;s/TeslaK40c/K40c/;s/TeslaK20m/K20m/;s/Intel-R-Xeon/Xeon/'`
--- plots/plot-ppx.sh
+++ plots/plot-ppx.sh
@@ -4,7 +4,7 @@ cat << EOF | gnuplot --persist
 w = 0.4
 h = 0.3
 k = 0.0
-nr = 2
+nr = 1
 nc = 1
 row(x) = ((nr-x-1)*h)+k
 col(x) = x*w
@@ -26,11 +26,11 @@ set yrange [62:95]
 set title '(a) Com-DBLP K=4K m=4K n=32'
 plot "< perl ./get_time.perl ppx-DBLP-K4K-m4K-n32 " using (\$1/3600):3 notitle with lines lw 4
 
-set origin col(0),row(1)
-set size w,h
+# set origin col(0),row(1)
+# set size w,h
 
-set xrange [-0.3:18.3]
-set yrange [53:83]
-set title '(b) Com-LiveJournal K=3K m=4K n=32'
-plot "< perl ./get_time.perl ppx-LJ-K3K-m4K-n32 " using (\$1/3600):3 notitle with lines lw 4
+# set xrange [-0.3:18.3]
+# set yrange [53:83]
+# set title '(b) Com-LiveJournal K=3K m=4K n=32'
+# plot "< perl ./get_time.perl ppx-LJ-K3K-m4K-n32 " using (\$1/3600):3 notitle with lines lw 4
 EOF
