f46594c785639f1ba21eaa8e4ffd49cd549354f9
Rutger Hofman <rutger@cs.vu.nl>
2017-06-14 16:06:47 +0200

    Maxwell in (a) and (b), Pascal in (c)

--- plots/plot-all-gpu-detail.sh
+++ plots/plot-all-gpu-detail.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-./plot-gpu-sweep.sh --title "(a) Titan-X(Pascal) K=1024 no vectorization" titanx-pascal-1024-strategies-w1 `ls dblp-K1024/TITANX-Pascal-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
-./plot-gpu-sweep.sh --title "(a) Titan-X(Pascal) K=1024 vector width=2" titanx-pascal-1024-strategies-w2 `ls dblp-K1024/TITANX-Pascal-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
-./plot-gpu-sweep.sh --title "(a) Titan-X(Pascal) K=1024 vector width=4" titanx-pascal-1024-strategies-w4 `ls dblp-K1024/TITANX-Pascal-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
+./plot-gpu-sweep.sh --title "(c) Titan-X(Pascal) K=1024 no vectorization" titanx-pascal-1024-strategies-w1 `ls dblp-K1024/TITANX-Pascal-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
+./plot-gpu-sweep.sh --title "(c) Titan-X(Pascal) K=1024 vector width=2" titanx-pascal-1024-strategies-w2 `ls dblp-K1024/TITANX-Pascal-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
+./plot-gpu-sweep.sh --title "(c) Titan-X(Pascal) K=1024 vector width=4" titanx-pascal-1024-strategies-w4 `ls dblp-K1024/TITANX-Pascal-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
 
-./plot-gpu-sweep.sh --title "(b) Titan-X(Pascal) K=2048 no vectorization" titanx-pascal-2048-strategies-w1 `ls dblp-K2048/TITANX-Pascal-K2048-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
-./plot-gpu-sweep.sh --title "(b) Titan-X(Pascal) K=2048 vector width=2" titanx-pascal-2048-strategies-w2 `ls dblp-K2048/TITANX-Pascal-K2048-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
-./plot-gpu-sweep.sh --title "(b) Titan-X(Pascal) K=2048 vector width=4" titanx-pascal-2048-strategies-w4 `ls dblp-K2048/TITANX-Pascal-K2048-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
+./plot-gpu-sweep.sh --title "(c) Titan-X(Pascal) K=2048 no vectorization" titanx-pascal-2048-strategies-w1 `ls dblp-K2048/TITANX-Pascal-K2048-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
+./plot-gpu-sweep.sh --title "(c) Titan-X(Pascal) K=2048 vector width=2" titanx-pascal-2048-strategies-w2 `ls dblp-K2048/TITANX-Pascal-K2048-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
+./plot-gpu-sweep.sh --title "(c) Titan-X(Pascal) K=2048 vector width=4" titanx-pascal-2048-strategies-w4 `ls dblp-K2048/TITANX-Pascal-K2048-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
 
-./plot-gpu-sweep.sh --title "(b) Titan-X(Maxwell) K=1024 no vectorization" titanx-maxwell-1024-strategies-w1 `ls dblp-K1024/GeForceGTXTITANX-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
-./plot-gpu-sweep.sh --title "(b) Titan-X(Maxwell) K=1024 vector width=2" titanx-maxwell-1024-strategies-w2 `ls dblp-K1024/GeForceGTXTITANX-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
-./plot-gpu-sweep.sh --title "(b) Titan-X(Maxwell) K=1024 vector width=4" titanx-maxwell-1024-strategies-w4 `ls dblp-K1024/GeForceGTXTITANX-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
+./plot-gpu-sweep.sh --title "(a) Titan-X(Maxwell) K=1024 no vectorization" titanx-maxwell-1024-strategies-w1 `ls dblp-K1024/GeForceGTXTITANX-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
+./plot-gpu-sweep.sh --title "(a) Titan-X(Maxwell) K=1024 vector width=2" titanx-maxwell-1024-strategies-w2 `ls dblp-K1024/GeForceGTXTITANX-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
+./plot-gpu-sweep.sh --title "(a) Titan-X(Maxwell) K=1024 vector width=4" titanx-maxwell-1024-strategies-w4 `ls dblp-K1024/GeForceGTXTITANX-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
 
 ./plot-gpu-sweep.sh --title "GTX980 K=1024 no vectorization" gtx980-1024-strategies-w1 `ls dblp-K1024/GeForceGTX980-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
 ./plot-gpu-sweep.sh --title "GTX980 K=1024 vector width=2" gtx980-1024-strategies-w2 `ls dblp-K1024/GeForceGTX980-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
 ./plot-gpu-sweep.sh --title "GTX980 K=1024 vector width=4" gtx980-1024-strategies-w4 `ls dblp-K1024/GeForceGTX980-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
 
-./plot-gpu-sweep.sh --title "(c) K40c K=1024 no vectorization" k40-1024-strategies-w1 `ls dblp-K1024/TeslaK40c-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
-./plot-gpu-sweep.sh --title "(c) K40c K=1024 vector width=2" k40-1024-strategies-w2 `ls dblp-K1024/TeslaK40c-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
-./plot-gpu-sweep.sh --title "(c) K40c K=1024 vector width=4" k40-1024-strategies-w4 `ls dblp-K1024/TeslaK40c-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
+./plot-gpu-sweep.sh --title "(d) K40c K=1024 no vectorization" k40-1024-strategies-w1 `ls dblp-K1024/TeslaK40c-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
+./plot-gpu-sweep.sh --title "(d) K40c K=1024 vector width=2" k40-1024-strategies-w2 `ls dblp-K1024/TeslaK40c-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
+./plot-gpu-sweep.sh --title "(d) K40c K=1024 vector width=4" k40-1024-strategies-w4 `ls dblp-K1024/TeslaK40c-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W4*`
 
 ./plot-gpu-sweep.sh --title "K20m K=1024 no vectorization" k20-1024-strategies-w1 `ls dblp-K1024/TeslaK20m-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W1*`
 ./plot-gpu-sweep.sh --title "K20m K=1024 vector width=2" k20-1024-strategies-w2 `ls dblp-K1024/TeslaK20m-K1024-M4096-N32-Snode-R0.01-A0.001-C0-*-W2*`
